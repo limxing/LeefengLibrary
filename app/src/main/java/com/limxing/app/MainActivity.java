c4f9c63b897b9d978fdf85d4e11b8989ee52cfe3
limxing <474631895@qq.com>
2015-11-24 14:43:01 +0800

    添加注释

--- app/src/main/java/com/limxing/app/MainActivity.java
+++ app/src/main/java/com/limxing/app/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends SwipeBackActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         SystemBarTintManager.initSystemBar(this, R.color.transparent);
 
 
