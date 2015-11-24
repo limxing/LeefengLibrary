7da04ad192e0f410ea7836dfb5b305d32c365177
limxing <474631895@qq.com>
2015-11-24 11:31:21 +0800

    添加沉浸式状态栏工具

--- app/src/main/java/com/limxing/app/MainActivity.java
+++ app/src/main/java/com/limxing/app/MainActivity.java
@@ -3,12 +3,16 @@ package com.limxing.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.limxing.utils.SystemBarTintManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SystemBarTintManager.initSystemBar(this,R.color.colorAccent);
 
 
 
