f6b8452d83f33f990fb73311063edcb8b71be7be
limxing <474631895@qq.com>
2015-11-30 15:10:33 +0800

    添加底部弹出窗口

--- library/src/main/java/com/limxing/library/BottomDialog/BottomDialog.java
+++ library/src/main/java/com/limxing/library/BottomDialog/BottomDialog.java
@@ -5,18 +5,33 @@ import android.app.Activity;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 
 import com.limxing.library.NoTitleBar.SystemBarTintManager;
 import com.limxing.library.R;
 import com.limxing.library.utils.LogUtils;
 
-public class BottomDialog extends Activity {
+public class BottomDialog extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         SystemBarTintManager.initSystemBar(this, R.color.transparent);
         setContentView(R.layout.bottomdialog);
+        findViewById(R.id.bottom_bac).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
     }
 }
