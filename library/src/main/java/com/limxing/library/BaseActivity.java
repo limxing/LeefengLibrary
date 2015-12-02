6dc82c871c5e5155a6a7efbda7db8a2103189449
limxing <474631895@qq.com>
2015-12-02 13:40:01 +0800

    添加一个activitybaseActivity

---
+++ library/src/main/java/com/limxing/library/BaseActivity.java
@@ -0,0 +1,44 @@
+package com.limxing.library;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.limxing.library.NoTitleBar.SystemBarTintManager;
+
+/**
+ * Created by limxing on 15/12/1.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SystemBarTintManager.initSystemBar(this, R.color.transparent);
+        initView();
+        init();
+    }
+
+    /**
+     * 初始化界面
+     */
+    protected abstract void initView();
+
+    /**
+     * 初始化数据
+     */
+    protected abstract void init();
+
+    // 切换界面动画开启一个Activiyty
+    protected void startActivityWithAnim(Intent intent) {
+        startActivity(intent);
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+    // 切换界面动画关闭一个Activiyty
+    protected void finishActivity() {
+        finish();
+        overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+}
