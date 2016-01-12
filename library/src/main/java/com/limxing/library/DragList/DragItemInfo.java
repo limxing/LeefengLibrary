cf3834dbf48dbdfd348998513525c333daf70a89
limxing <474631895@qq.com>
2016-01-12 15:36:22 +0800

    添加拖动排序的ListView

---
+++ library/src/main/java/com/limxing/library/DragList/DragItemInfo.java
@@ -0,0 +1,14 @@
+package com.limxing.library.DragList;
+
+import android.widget.ImageView;
+
+/**
+ * 用来记录拖动Item信息的实体类
+ *
+ * @author zihao
+ *
+ */
+public class DragItemInfo {
+	public Object obj;
+	public ImageView dragImgView;
+}
