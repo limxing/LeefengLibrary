bfc7196a2e60b46db71d6c27479093789df99536
limxing <474631895@qq.com>
2016-02-29 22:13:42 +0800

    添加phone信息打印

---
+++ library/src/main/java/com/limxing/library/utils/PhoneInfo.java
@@ -0,0 +1,24 @@
+package com.limxing.library.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by limxing on 16/2/29.
+ */
+public class PhoneInfo {
+
+    public static void show(Activity context){
+        DisplayMetrics dm = new DisplayMetrics();
+        context.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int widthPixels= dm.widthPixels;
+        int heightPixels= dm.heightPixels;
+        float density = dm.density;
+        int densityDpi = dm.densityDpi;
+        int screenWidth = (int) (widthPixels * density);
+        int screenHeight = (int) (heightPixels * density);
+        LogUtils.i("screenWidth:"+screenWidth+"screenHeight:"+screenHeight+"densityDpi:"+densityDpi
+                +"densityDpi:"+density);
+    }
+}
