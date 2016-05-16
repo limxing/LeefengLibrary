0c0f66610ed0287d618420fd39448cf370759b7a
limxing <474631895@qq.com>
2016-05-16 16:40:57 +0800

    完善时间选择器

--- library/src/main/java/com/limxing/library/CustomDialog/DataDialog.java
+++ library/src/main/java/com/limxing/library/CustomDialog/DataDialog.java
@@ -2,12 +2,15 @@ package com.limxing.library.CustomDialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.limxing.library.LoopView.LoopView;
 import com.limxing.library.LoopView.OnItemSelectedListener;
@@ -19,7 +22,8 @@ import java.util.List;
 /**
  * Created by limxing on 16/5/16.
  */
-public class DataDialog implements OnItemSelectedListener {
+public class DataDialog implements OnItemSelectedListener, View.OnClickListener, DialogInterface.OnDismissListener {
+    private  TextView confirmText;
     private RelativeLayout relativeLayout;
     private DataDialogListener dataDialogListener;
     private Context context;
@@ -45,6 +49,16 @@ public class DataDialog implements OnItemSelectedListener {
         yearView.setListener(this);
         monthView.setListener(this);
         dayView.setListener(this);
+        confirmText = new TextView(context);
+        confirmText.setText("确定");
+        confirmText.setTextColor(Color.parseColor("#7bbfea"));
+        confirmText.setTextSize(18);
+        confirmText.setOnClickListener(this);
+        RelativeLayout.LayoutParams lp1 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp1.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+//        lp1.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+        lp1.setMargins(0, 15, 35, 0);
+        relativeLayout.addView(confirmText, lp1);
 
     }
 
@@ -56,27 +70,39 @@ public class DataDialog implements OnItemSelectedListener {
         yearView.measure(0, 0);
         monthView.measure(0, 0);
         dayView.measure(0, 0);
+        confirmText.measure(0, 0);
+        int textH = confirmText.getMeasuredHeight() + 30;
+
+        View view = new View(context);
+        view.setBackgroundColor(Color.parseColor("#eeeeee"));
+        RelativeLayout.LayoutParams lp1 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 2);
+//        lp1.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+//        lp1.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+        lp1.setMargins(0, textH, 0, 0);
+        relativeLayout.addView(view, lp1);
+        textH = textH + 32;
+
         int yearW = yearView.getMeasuredWidth();
         int monthW = monthView.getMeasuredWidth();
         int dayW = dayView.getMeasuredWidth();
         int padleft = DisplayUtil.getScreenWith(context) / 2 - (yearW + monthW + dayW + 100) / 2;
 
         ViewGroup.MarginLayoutParams mp = new ViewGroup.MarginLayoutParams(yearW, ViewGroup.LayoutParams.WRAP_CONTENT);  //item的宽高
-        mp.setMargins(padleft, 0, 0, 0);//分别是margin_top那四个属性
+        mp.setMargins(padleft, textH, 0, 30);//分别是margin_top那四个属性
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(mp);
         yearView.setLayoutParams(params);
         relativeLayout.addView(yearView);
 
         padleft = padleft + yearW + 50;
         mp = new ViewGroup.MarginLayoutParams(monthW, ViewGroup.LayoutParams.WRAP_CONTENT);  //item的宽高
-        mp.setMargins(padleft, 0, 0, 0);//分别是margin_top那四个属性
+        mp.setMargins(padleft, textH, 0, 30);//分别是margin_top那四个属性
         params = new RelativeLayout.LayoutParams(mp);
         monthView.setLayoutParams(params);
         relativeLayout.addView(monthView);
 
         padleft = padleft + monthW + 50;
         mp = new ViewGroup.MarginLayoutParams(dayW, ViewGroup.LayoutParams.WRAP_CONTENT);  //item的宽高
-        mp.setMargins(padleft, 0, 0, 0);//分别是margin_top那四个属性
+        mp.setMargins(padleft, textH, 0, 30);//分别是margin_top那四个属性
         params = new RelativeLayout.LayoutParams(mp);
         dayView.setLayoutParams(params);
         relativeLayout.addView(dayView);
@@ -86,6 +112,7 @@ public class DataDialog implements OnItemSelectedListener {
 
     public void show() {
         dialog = new Dialog(context, R.style.MMTheme_DataSheet);
+        dialog.setOnDismissListener(this);
         dialog.setCanceledOnTouchOutside(true);
         dialog.setContentView(relativeLayout);
         Window w = dialog.getWindow();
@@ -101,10 +128,27 @@ public class DataDialog implements OnItemSelectedListener {
     public void onItemSelected(int view) {
         String year = yearView.getItems().get(yearView.getSelectedItem());
         String month = monthView.getItems().get(monthView.getSelectedItem());
-        String day = dayView.getItems().get(dayView.getSelectedItem());
+        String day = "";
+        if (dayView.getItems() != null) {
+            day = dayView.getItems().get(dayView.getSelectedItem());
+        }
         if (dataDialogListener != null) {
             dataDialogListener.onItemSelected(year, month, day);
         }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        onItemSelected(0);
+        dialog.dismiss();
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialogInterface) {
+        context=null;
+        dataDialogListener=null;
+        dialog=null;
 
     }
 
