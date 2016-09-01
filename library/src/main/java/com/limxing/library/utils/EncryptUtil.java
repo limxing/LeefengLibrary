80b95d295c6ba863442ee976e83126892821cc60
Lifeng Li <474631895@qq.com>
2016-09-01 17:24:55 +0800

    Update EncryptUtil.java

--- library/src/main/java/com/limxing/library/utils/EncryptUtil.java
+++ library/src/main/java/com/limxing/library/utils/EncryptUtil.java
@@ -53,7 +53,46 @@ public class EncryptUtil {
     }
     private final static String[] hexDigits = { "0", "1", "2", "3", "4", "5",
             "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
-
+ /**
+     * MD5计算
+     *
+     * @param e
+     * @return
+     * @throws Exception
+     */
+    public static String fileToMD5(File e) throws Exception {
+        String md5 = null;
+        MessageDigest md = null;
+        FileInputStream in = null;
+        String var9;
+        try {
+            if (e.exists() && e.isFile()) {
+                byte[] buffer = new byte[1024];
+                boolean len = false;
+                md = MessageDigest.getInstance("MD5");
+                in = new FileInputStream(e);
+                int len1;
+                while ((len1 = in.read(buffer)) != -1) {
+                    md.update(buffer, 0, len1);
+                }
+                BigInteger bigInt = new BigInteger(1, md.digest());
+                md5 = bigInt.toString(16);
+            }
+            var9 = md5;
+        } catch (Exception var16) {
+            var16.printStackTrace();
+            throw var16;
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException var15) {
+                var15.printStackTrace();
+            }
+        }
+        return var9;
+    }
 }
 
 ///**
