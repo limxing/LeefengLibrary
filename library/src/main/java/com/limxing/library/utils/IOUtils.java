69def7380549543fb6e51fb06412457c7ab17e44
Lifeng Li <474631895@qq.com>
2016-09-01 17:28:18 +0800

    Update IOUtils.java

--- library/src/main/java/com/limxing/library/utils/IOUtils.java
+++ library/src/main/java/com/limxing/library/utils/IOUtils.java
@@ -19,6 +19,30 @@ public class IOUtils {
 		return true;
 	}
 	/**
+	 * 将输入流转换成字符串
+	 *
+	 * @param is
+	 *            从网络获取的输入流
+	 * @return
+	 */
+	public static String streamToString(InputStream is) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			while ((len = is.read(buffer)) != -1) {
+				baos.write(buffer, 0, len);
+			}
+			baos.close();
+			is.close();
+			byte[] byteArray = baos.toByteArray();
+			return new String(byteArray);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	/**
 	 * 读流转为成字符串
 	 * @param in
 	 * @return
