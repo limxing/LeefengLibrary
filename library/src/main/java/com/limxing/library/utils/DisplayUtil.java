b0a325e7be4c7ab3c8e36cf2d273c738e5533b00
Lifeng Li <474631895@qq.com>
2016-09-02 10:54:30 +0800

    Update DisplayUtil.java

--- library/src/main/java/com/limxing/library/utils/DisplayUtil.java
+++ library/src/main/java/com/limxing/library/utils/DisplayUtil.java
@@ -8,108 +8,125 @@ import android.view.WindowManager;
 
 
 /**
- *
- *
  * 作者： limxing
- *
+ * <p/>
  * 时间： 2015-10-30 上午10:51:16
- *
+ * <p/>
  * 描述：屏幕像素pxps dp之间的转换
- *
  */
 public class DisplayUtil {
 
-	/**
-	 * 将px值转换为dip或dp值，保证尺寸大小不变
-	 *
-	 * @param pxValue
-	 *
-	 *            （DisplayMetrics类中属性density）
-	 * @return
-	 */
-	public static int px2dip(Context context, float pxValue) {
-		final float scale = context.getResources().getDisplayMetrics().density;
-		return (int) (pxValue / scale + 0.5f);
-	}
+    private static int screenWidth;
+    private static int screenHeight;
 
-	/**
-	 * 将dip或dp值转换为px值，保证尺寸大小不变
-	 *
-	 * @param dipValue
-	 *
-	 *            （DisplayMetrics类中属性density）
-	 * @return
-	 */
-	public static int dip2px(Context context, float dipValue) {
-		final float scale = context.getResources().getDisplayMetrics().density;
-		return (int) (dipValue * scale + 0.5f);
-	}
+    /**
+     * 将px值转换为dip或dp值，保证尺寸大小不变
+     *
+     * @param pxValue （DisplayMetrics类中属性density）
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
 
-	/**
-	 * 将px值转换为sp值，保证文字大小不变
-	 *
-	 * @param pxValue
-	 *
-	 *            （DisplayMetrics类中属性scaledDensity）
-	 * @return
-	 */
-	public static int px2sp(Context context, float pxValue) {
-		final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
-		return (int) (pxValue / fontScale + 0.5f);
-	}
+    /**
+     * 将dip或dp值转换为px值，保证尺寸大小不变
+     *
+     * @param dipValue （DisplayMetrics类中属性density）
+     * @return
+     */
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
 
-	/**
-	 * 将sp值转换为px值，保证文字大小不变
-	 *
-	 * @param spValue
-	 *
-	 *            （DisplayMetrics类中属性scaledDensity）
-	 * @return
-	 */
-	public static int sp2px(Context context, float spValue) {
-		final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
-		return (int) (spValue * fontScale + 0.5f);
-	}
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     *
+     * @param pxValue （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
 
-	public static int getFontHeight(float fontSize) {
-		Paint paint = new Paint();
-		paint.setTextSize(fontSize);
-		FontMetrics fm = paint.getFontMetrics();
-		return (int) Math.ceil(fm.descent - fm.ascent);
-	}
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *
+     * @param spValue （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
 
-	/**
-	 * 获取屏幕宽度的方法
-	 * @param context
-	 * @return
-	 */
-	public static int getScreenWith(Context context){
-		WindowManager wm = (WindowManager) context
-				.getSystemService(Context.WINDOW_SERVICE);
-		return wm.getDefaultDisplay().getWidth();
-	}
-	/**
-	 * 获取屏幕高度的方法
-	 * @param context
-	 * @return
-	 */
-	public static int getScreenHeight(Context context){
-		WindowManager wm = (WindowManager) context
-				.getSystemService(Context.WINDOW_SERVICE);
-		return wm.getDefaultDisplay().getHeight();
-	}
+    public static int getFontHeight(float fontSize) {
+        Paint paint = new Paint();
+        paint.setTextSize(fontSize);
+        FontMetrics fm = paint.getFontMetrics();
+        return (int) Math.ceil(fm.descent - fm.ascent);
+    }
 
-	/**
-	 * 获取 NavigationBar 的高度
-	 * @param context
-	 * @return
-	 */
-	public static int getNavigationBarHeight(Context context) {
-		Resources resources = context.getResources();
-		int resourceId = resources.getIdentifier("navigation_bar_height",
-				"dimen", "android");
-		//获取NavigationBar的高度
-		int height = resources.getDimensionPixelSize(resourceId);
-		return height;
-	}
+
+    /**
+     * 获取屏幕信息
+     *
+     * @param context
+     * @return
+     */
+    public static int[] getScreenWH(Context context) {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        return new int[]{wm.getDefaultDisplay().getWidth(), wm.getDefaultDisplay().getHeight()};
+    }
+
+    /**
+     * 获取屏幕宽度的方法
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenWith(Context context) {
+        if (screenWidth == 0) {
+            WindowManager wm = (WindowManager) context
+                    .getSystemService(Context.WINDOW_SERVICE);
+            screenWidth = wm.getDefaultDisplay().getWidth();
+        }
+
+        return screenWidth;
+    }
+
+
+    /**
+     * 获取屏幕高度的方法
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        if (screenHeight == 0) {
+            WindowManager wm = (WindowManager) context
+                    .getSystemService(Context.WINDOW_SERVICE);
+            screenHeight = wm.getDefaultDisplay().getHeight();
+        }
+        return screenHeight;
+    }
+
+    /**
+     * 获取 NavigationBar 的高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getNavigationBarHeight(Context context) {
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height",
+                "dimen", "android");
+        //获取NavigationBar的高度
+        int height = resources.getDimensionPixelSize(resourceId);
+        return height;
+    }
 }
