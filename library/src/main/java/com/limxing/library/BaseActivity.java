00b2a061beb22d432d6e14cfa7cbb9e42fe7832a
limxing <474631895@qq.com>
2016-11-14 16:07:39 +0800

    封装findviewbyid

--- library/src/main/java/com/limxing/library/BaseActivity.java
+++ library/src/main/java/com/limxing/library/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.limxing.library;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.limxing.library.NoTitleBar.SystemBarTintManager;
 import com.limxing.library.Permission.CheckPermListener;
@@ -135,4 +138,72 @@ public abstract class BaseActivity extends AppCompatActivity implements
             inputmanger.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
+
+
+    /**
+     * 把findViewById封装
+     */
+    class Query {
+        private final Activity activity;
+        private View view;
+
+        public Query(Activity activity) {
+            this.activity = activity;
+        }
+
+        public Query id(int id) {
+            view = activity.findViewById(id);
+            return this;
+        }
+
+        public Query image(int resId) {
+            if (view instanceof ImageView) {
+                ((ImageView) view).setImageResource(resId);
+            }
+            return this;
+        }
+
+        public Query visible() {
+            if (view != null) {
+                view.setVisibility(View.VISIBLE);
+            }
+            return this;
+        }
+
+        public Query gone() {
+            if (view != null) {
+                view.setVisibility(View.GONE);
+            }
+            return this;
+        }
+
+        public Query invisible() {
+            if (view != null) {
+                view.setVisibility(View.INVISIBLE);
+            }
+            return this;
+        }
+
+        public Query clicked(View.OnClickListener handler) {
+            if (view != null) {
+                view.setOnClickListener(handler);
+            }
+            return this;
+        }
+
+        public Query text(CharSequence text) {
+            if (view != null && view instanceof TextView) {
+                ((TextView) view).setText(text);
+            }
+            return this;
+        }
+
+        public Query visibility(int visible) {
+            if (view != null) {
+                view.setVisibility(visible);
+            }
+            return this;
+        }
+    }
+
 }
