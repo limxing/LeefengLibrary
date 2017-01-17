6575bb9fc7882060f99afb31e31b3e1658814fa1
limxing <474631895@qq.com>
2017-01-17 15:56:50 +0800

    添加PlayImageView

---
+++ library/src/main/java/com/limxing/library/view/PlayItemView.java
@@ -0,0 +1,44 @@
+package com.limxing.library.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.limxing.library.R;
+
+/**
+ * Created by limxing on 2017/1/17.
+ */
+
+public class PlayItemView extends ImageView {
+    private boolean isPlay;
+
+    public PlayItemView(Context context) {
+        super(context);
+    }
+
+    public PlayItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (isPlay) {
+            int w = getMeasuredWidth();
+            int h = getMeasuredHeight();
+            Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.leefeng_icon_play);
+            canvas.drawBitmap(bitmap, null, new Rect(0, 0, w, h), null);
+        }
+    }
+
+    public void setPlay(boolean play) {
+        isPlay = play;
+        postInvalidate();
+    }
+}
