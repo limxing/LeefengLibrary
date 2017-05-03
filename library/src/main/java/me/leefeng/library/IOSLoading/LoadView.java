55121e85a48c43379d4d8584f9c794f9d6205926
limxing <474631895@qq.com>
2017-05-03 13:50:30 +0800

    使用valueAnimator

--- library/src/main/java/me/leefeng/library/IOSLoading/LoadView.java
+++ library/src/main/java/me/leefeng/library/IOSLoading/LoadView.java
@@ -1,27 +1,27 @@
 package me.leefeng.library.IOSLoading;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Matrix;
 
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
-
-
 
 
 import java.lang.ref.SoftReference;
 
 import me.leefeng.library.R;
+import me.leefeng.library.utils.LogUtils;
 
 /**
  * Created by limxing on 16/1/7.
  */
 public class LoadView extends ImageView {
-    private MyRunable runnable;
     private int width;
     private int height;
-    private Drawable drawable;
+    private ValueAnimator animator;
 
     public LoadView(Context context) {
         super(context);
@@ -39,33 +39,27 @@ public class LoadView extends ImageView {
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         init();
-        runnable.startload();
     }
 
     /**
-     *
-     * @param res
      */
-    public void setDrawable(int res) {
-        runnable.stopload();
-        setImageDrawable(getResources().getDrawable(res));
-    }
-
-    public void startLoading() {
+    public void setDrawable(Drawable drawable) {
         setImageDrawable(drawable);
-        runnable.startload();
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
-        runnable.stopload();
+        if (animator != null)
+            animator.cancel();
+        animator = null;
+//        runnable.stopload();
 
     }
 
     private void init() {
         setScaleType(ScaleType.MATRIX);
-        drawable = getDrawable();
+        Drawable drawable = getDrawable();
         if (drawable == null) {
             drawable = getResources().getDrawable(R.drawable.svpload);
             setImageDrawable(drawable);
@@ -73,49 +67,69 @@ public class LoadView extends ImageView {
         measure(0, 0);
         width = getMeasuredWidth() / 2;
         height = getMeasuredHeight() / 2;
-        runnable = new MyRunable(this);
+        start();
     }
 
+    private Matrix max;
 
-    static class MyRunable implements Runnable {
-        private boolean flag;
-        private SoftReference<LoadView> loadingViewSoftReference;
-        private float degrees = 0f;
-        private Matrix max;
-
-        public MyRunable(LoadView loadingView) {
-            loadingViewSoftReference = new SoftReference<LoadView>(loadingView);
-            max = new Matrix();
-        }
-
-        @Override
-        public void run() {
-            if (loadingViewSoftReference.get().runnable != null && max != null) {
-                degrees += 30f;
-                max.setRotate(degrees, loadingViewSoftReference.get().width, loadingViewSoftReference.get().height);
-                loadingViewSoftReference.get().setImageMatrix(max);
-                if (degrees == 360) {
-                    degrees = 0;
-                }
-                if (flag) {
-                    loadingViewSoftReference.get().postDelayed(loadingViewSoftReference.get().runnable, 80);
-                } else {
-                    max.setRotate(0, loadingViewSoftReference.get().width, loadingViewSoftReference.get().height);
-                    loadingViewSoftReference.get().setImageMatrix(max);
-                }
+    private void start() {
+        max = new Matrix();
+        animator = ValueAnimator.ofInt(0, 12);
+        animator.setDuration(12 * 80);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatCount(Integer.MAX_VALUE);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                LogUtils.i("==" + valueAnimator.getAnimatedValue());
+                float degrees = 30 * Integer.parseInt(valueAnimator.getAnimatedValue().toString());
+                max.setRotate(degrees, width, height);
+                setImageMatrix(max);
             }
-        }
-
-        public void stopload() {
-            flag = false;
-
-        }
-
-        public void startload() {
-            flag = true;
-            if (loadingViewSoftReference.get().runnable != null && max != null) {
-                loadingViewSoftReference.get().postDelayed(loadingViewSoftReference.get().runnable, 80);
-            }
-        }
+        });
+        animator.start();
     }
+
+//    static class MyRunable implements Runnable {
+//        private boolean flag;
+//        private SoftReference<LoadView> loadingViewSoftReference;
+//        private float degrees = 0f;
+//        private Matrix max;
+//
+//        public MyRunable(LoadView loadingView) {
+//            loadingViewSoftReference = new SoftReference<LoadView>(loadingView);
+//            max = new Matrix();
+//        }
+//
+//        @Override
+//        public void run() {
+//            if (loadingViewSoftReference.get().runnable != null && max != null) {
+//                degrees += 30f;
+//                max.setRotate(degrees, loadingViewSoftReference.get().width, loadingViewSoftReference.get().height);
+//                loadingViewSoftReference.get().setImageMatrix(max);
+//                if (degrees == 360) {
+//                    degrees = 0;
+//                }
+//                if (flag) {
+//                    loadingViewSoftReference.get().postDelayed(loadingViewSoftReference.get().runnable, 80);
+//                } else {
+//                    max.setRotate(0, loadingViewSoftReference.get().width, loadingViewSoftReference.get().height);
+//                    loadingViewSoftReference.get().setImageMatrix(max);
+//                }
+//            }
+//        }
+//
+//        public void stopload() {
+//            flag = false;
+//
+//        }
+//
+//        public void startload() {
+//            flag = true;
+//            if (loadingViewSoftReference.get().runnable != null && max != null) {
+//                loadingViewSoftReference.get().postDelayed(loadingViewSoftReference.get().runnable, 80);
+//            }
+//        }
+//    }
+
 }
